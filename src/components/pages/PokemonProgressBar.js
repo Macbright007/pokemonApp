df628f28dc9abc8732e8b1c53b803b3d208395c5
Macbright007 <nbright239@gmail.com>
2022-09-27 02:49:09 +0100

    responsive design changes

--- src/components/pages/PokemonProgressBar.js
+++ src/components/pages/PokemonProgressBar.js
@@ -4,10 +4,12 @@ import PokemonContext from "../../contexts/PokemonContext";
 import { useContext } from "react";
 
 const PokemonProgressBar = ({ pokemon }) => {
-  const { addToFavorite, favouritePokemon, removeFromFavourite } = useContext(PokemonContext);
-  const isFav = favouritePokemon.filter(fav => fav.id === pokemon.id).length > 0
+  const { addToFavorite, favouritePokemon, removeFromFavourite } =
+    useContext(PokemonContext);
+  const isFav =
+    favouritePokemon.filter((fav) => fav.id === pokemon.id).length > 0;
   // favouritePokemon.findIndex(fav => fav.id === pokemon.id) > -1
-  console.log('isFav', isFav)
+  console.log("isFav", isFav);
   return (
     <div className="progress_cont">
       <div className="stats">
@@ -15,86 +17,103 @@ const PokemonProgressBar = ({ pokemon }) => {
           {pokemon.stats[0].stat.name}
           <span className="inner__stat">{pokemon.stats[0].base_stat}</span>
         </p>
-        <ProgressBar
-          completed={pokemon.stats[0].base_stat}
-          width="350"
-          height={8}
-          bgColor="#CD2873"
-          isLabelVisible={false}
-        />
+        <div className="prog__bar">
+          <ProgressBar
+            completed={pokemon.stats[0].base_stat}
+            width="350"
+            height={8}
+            bgColor="#CD2873"
+            isLabelVisible={false}
+          />
+        </div>
       </div>
       <div className="stats">
         <p>
           {pokemon.stats[1].stat.name}
           <span className="inner__stat">{pokemon.stats[1].base_stat}</span>
         </p>
-        <ProgressBar
-          completed={pokemon.stats[1].base_stat}
-          width="350"
-          height={8}
-          bgColor="#CD2873"
-          isLabelVisible={false}
-        />
+        <div className="prog__bar">
+          <ProgressBar
+            completed={pokemon.stats[1].base_stat}
+            width="350"
+            height={8}
+            bgColor="#CD2873"
+            isLabelVisible={false}
+          />
+        </div>
       </div>
       <div className="stats">
         <p>
           {pokemon.stats[2].stat.name}
           <span className="inner__stat">{pokemon.stats[2].base_stat}</span>
         </p>
-        <ProgressBar
-          completed={pokemon.stats[2].base_stat}
-          width="350"
-          height={8}
-          bgColor="#CD2873"
-          isLabelVisible={false}
-        />
+        <div className="prog__bar">
+          <ProgressBar
+            completed={pokemon.stats[2].base_stat}
+            width="350"
+            height={8}
+            bgColor="#CD2873"
+            isLabelVisible={false}
+          />
+        </div>
       </div>
       <div className="stats">
         <p>
           {pokemon.stats[3].stat.name}
           <span className="inner__stat">{pokemon.stats[3].base_stat}</span>
         </p>
-        <ProgressBar
-          completed={pokemon.stats[3].base_stat}
-          width="350"
-          height={8}
-          bgColor="#EEC218"
-          isLabelVisible={false}
-        />
+        <div className="prog__bar">
+          <ProgressBar
+            completed={pokemon.stats[3].base_stat}
+            width="350"
+            height={8}
+            bgColor="#EEC218"
+            isLabelVisible={false}
+          />
+        </div>
       </div>
       <div className="stats">
         <p>
           {pokemon.stats[4].stat.name}
           <span className="inner__stat">{pokemon.stats[4].base_stat}</span>
         </p>
-        <ProgressBar
-          completed={pokemon.stats[4].base_stat}
-          width="350"
-          height={8}
-          bgColor="#EEC218"
-          isLabelVisible={false}
-        />
+        <div className="prog__bar">
+          <ProgressBar
+            completed={pokemon.stats[4].base_stat}
+            width="350"
+            height={8}
+            bgColor="#EEC218"
+            isLabelVisible={false}
+          />
+        </div>
       </div>
       <div className="stats">
         <p>
           {pokemon.stats[5].stat.name}
           <span className="inner__stat">{pokemon.stats[5].base_stat}</span>
         </p>
-        <ProgressBar
-          completed={pokemon.stats[5].base_stat}
-          width="350"
-          height={8}
-          bgColor="#CD2873"
-          isLabelVisible={false}
-        />
+        <div className="prog__bar">
+          <ProgressBar
+            completed={pokemon.stats[5].base_stat}
+            width="350"
+            height={8}
+            bgColor="#CD2873"
+            isLabelVisible={false}
+          />
+        </div>
       </div>
       <div className="btn__cont">
         {!isFav ? (
           <button className="btn__btn" onClick={() => addToFavorite(pokemon)}>
             Mark as favourites
           </button>
-         ) : (
-          <button className="btnn" onClick={() => removeFromFavourite(pokemon.id)} >Remove from favourites</button>
+        ) : (
+          <button
+            className="btnn"
+            onClick={() => removeFromFavourite(pokemon.id)}
+          >
+            Remove from favourites
+          </button>
         )}
       </div>
     </div>
